2c92119d8617a9849cc736fc68f25c5635c5cd56
Kayashov.SM <kayshovcm@answer-42.ru>
2023-05-13 00:48:34 +0300

    1.1.1

---
+++ src/main/java/ru/kayashov/storage/anotation/CarRepository.java
@@ -0,0 +1,18 @@
+package ru.kayashov.storage.anotation;
+
+import org.springframework.stereotype.Repository;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Repository
+public @interface CarRepository {
+
+    String name();
+}
